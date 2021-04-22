1bb6841456944f8ce425df47be5fe06fbd7c7cb7
albertsahu92 <54240284+albertsahu92@users.noreply.github.com>
2021-04-22 22:26:39 +0700

    created new vue file

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About this app</h1>
+    <v-subtitle> <p> lorem ipsum dolor sit amet lorem ipsum dolor dolorum dolor</p>
+    <p> lorem ipsum dolor sit amet lorem ipsum dolor dolorum dolor
+      lorem ipsum dolor sit amet lorem ipsum dolor dolorum dolor
+      lorem ipsum dolor sit amet lorem ipsum dolor dolorum dolor
+    </p>
+    <p> lorem ipsum dolor sit amet lorem ipsum dolor dolorum dolor</p>
+    
+    
+     </v-subtitle>
   </div>
 </template>
